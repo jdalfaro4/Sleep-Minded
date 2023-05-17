113b95ef3598408fb88a1d6edb9d804c72e6d7ab
poguet <118129483+poguet@users.noreply.github.com>
2023-05-16 23:01:27 -0500

    Add files via upload

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,20 @@
+const { quality, Duration } = require('../models');
+
+const resolvers = {
+  Query: {
+    quality: async () => {
+      return quality.find({});
+    },
+    Duration: async (parent, { _id }) => {
+      const params = _id ? { _id } : {};
+      return Duration.find(params);
+    },
+  },
+  Mutation: {
+    createDuration: async (parent, args) => {
+      const Duration = await Duration.create(args);
+      return Duration;
+    }
+   }
+}
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,25 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type quality {
+    _id: ID!
+    name: String!
+  }
+
+  type Duration {
+    _id: ID!
+    sleepHours: String!
+    sleepQuality: String!
+  }
+
+  type Query {
+  //  tech: [Tech] //
+    Duration(_id: String): [Durtion]
+  }
+
+  type Mutation {
+    createDuration(sleepHours: String!, sleepQuality: String!): duration
+  }
+`;
+
+module.exports = typeDef
