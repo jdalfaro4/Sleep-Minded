93dee7bd074e7589a8b58a09ed9c8db0c4495674
jdalfaro4 <jdalfaro4@gmail.com>
2023-05-17 21:35:48 -0500

    updating routes for home page

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CreateAccountPage from './CreateAccountPage';
+import LoginPage from './LoginPage';
+import SleepInputPage from './SleepInputPage';
+import DashboardPage from './DashboardPage';
 
 const HomePage = () => {
+
+  const [activeComponent, setActiveComponent] = useState('');
+
+  const handleNavigationClick = (componentName) => {
+      setActiveComponent(componentName);
+  };
+  let componentToRender;
+  switch (activeComponent) {
+      case 'CreateAccountPage':
+          componentToRender = <CreateAccountPage />;
+          break;
+      case 'LoginPage':
+          componentToRender = <LoginPage />;
+          break;
+      case 'SleepInputPage':
+          componentToRender = <SleepInputPage />;
+          break;
+      case 'DashboardPage':
+          componentToRender = <DashboardPage />;
+          break;
+          default:
+            componentToRender = <LoginPage />;
+  }
+
   return (
     <div className="homepage">
       <button>Login</button>
       <button>SleepInput</button>
       <button>Dashboard</button>
       <button>Logout</button>
+      {componentToRender}
     </div>
   );
 };
