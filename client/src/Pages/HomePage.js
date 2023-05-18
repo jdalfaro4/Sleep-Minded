447ba6598d054d2f9897ce8fc2c9536529654f38
Cj adorna <cjadorna@hotmail.com>
2023-05-18 12:25:54 -0500

    fixed buttons/ react rendering

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -1,40 +1,42 @@
 import React, { useState } from 'react';
-import CreateAccountPage from './CreateAccountPage';
+// import CreateAccountPage from './CreateAccountPage';
 import LoginPage from './LoginPage';
 import SleepInputPage from './SleepInputPage';
 import DashboardPage from './DashboardPage';
 
 const HomePage = () => {
-
-  const [activeComponent, setActiveComponent] = useState('');
+  const [activeComponent, setActiveComponent] = useState('LoginPage');
 
   const handleNavigationClick = (componentName) => {
-      setActiveComponent(componentName);
+    setActiveComponent(componentName);
   };
+
   let componentToRender;
   switch (activeComponent) {
-      case 'CreateAccountPage':
-          componentToRender = <CreateAccountPage />;
-          break;
-      case 'LoginPage':
-          componentToRender = <LoginPage />;
-          break;
-      case 'SleepInputPage':
-          componentToRender = <SleepInputPage />;
-          break;
-      case 'DashboardPage':
-          componentToRender = <DashboardPage />;
-          break;
-          default:
-            componentToRender = <LoginPage />;
+    // case 'CreateAccountPage':
+    //   componentToRender = <CreateAccountPage />;
+    //   break;
+    case 'LoginPage':
+      componentToRender = <LoginPage />;
+      break;
+    case 'SleepInputPage':
+      componentToRender = <SleepInputPage />;
+      break;
+    case 'DashboardPage':
+      componentToRender = <DashboardPage />;
+      break;
+    default:
+      componentToRender = <LoginPage />;
   }
 
   return (
     <div className="homepage">
-      <button>Login</button>
-      <button>SleepInput</button>
-      <button>Dashboard</button>
-      <button>Logout</button>
+      <div className="navbar">
+        {/* <button onClick={() => handleNavigationClick('LoginPage')}>Login</button> */}
+        <button onClick={() => handleNavigationClick('SleepInputPage')}>SleepInput</button>
+        <button onClick={() => handleNavigationClick('DashboardPage')}>Dashboard</button>
+        <button onClick={() => handleNavigationClick('LoginPage')}>Logout</button>
+      </div>
       {componentToRender}
     </div>
   );
