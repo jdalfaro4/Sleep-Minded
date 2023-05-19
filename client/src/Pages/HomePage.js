6e9057991313b91dd4a62ce5c1029ce1e16864a0
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-05-18 21:18:40 -0500

    bootstrap installed and navbar added to HomePage

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 import LoginPage from './LoginPage';
 import SleepInputPage from './SleepInputPage';
 import Chart from './Chart';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../App.css";
 
 const HomePage = () => {
   const [activeComponent, setActiveComponent] = useState('LoginPage');
@@ -30,12 +32,29 @@ const HomePage = () => {
   }
 
   return (
-    <div className="homepage">
-      <div className="navbar">
-        {/* <button onClick={() => handleNavigationClick('LoginPage')}>Login</button> */}
-        <button onClick={() => handleNavigationClick('SleepInputPage')}>SleepInput</button>
-        <button onClick={() => handleNavigationClick('Chart')}>Chart</button>
-        <button onClick={() => handleNavigationClick('LoginPage')}>Logout</button>
+    <div className="homepage background-image">
+      <div>
+
+        <nav className="navbar navbar-expand-lg sticky-top back">
+          <a className="navbar-brand km">SleepMinded</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" style={{ color: "white" }} id="contact" onClick={() => handleNavigationClick('SleepInputPage')}>Sleep Input</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" style={{ color: "white" }} id="portfolio" onClick={() => handleNavigationClick('Chart')}>Chart</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" style={{ color: "white" }} id="resume" onClick={() => handleNavigationClick('LoginPage')}>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
       </div>
       {componentToRender}
     </div>
@@ -43,3 +62,5 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
+
