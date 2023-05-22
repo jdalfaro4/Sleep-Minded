22688af32d3c33142124fd95c8f19fff8e416148
jdalfaro4 <jdalfaro4@gmail.com>
2023-05-22 18:33:56 -0500

    updated server client connection

--- client/src/Pages/Chart.js
+++ client/src/Pages/Chart.js
@@ -5,7 +5,7 @@
 import React from 'react';
 import { Line } from "react-chartjs-2";
 const xValues = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
-const yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
+const yValues = [8, 9, 7, 8, 6, 9, 6, 7, 8, 9, 8, 6];
 
 
 export default function Chart() {
