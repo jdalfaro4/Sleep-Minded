261e7335b8fdc7a4b799956922c86da0af99b7c0
JudgedGeo <115055273+Judgedgeo@users.noreply.github.com>
2023-05-24 16:07:19 -0500

    fixed log in

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,8 +3,10 @@ import { gql } from "@apollo/client";
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
-      user
-      _id
+      token
+      user {
+        email
+      }
     }
   }
 `;
